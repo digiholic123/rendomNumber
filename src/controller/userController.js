4f2cc0d6e1b7c0eb68f6d7820a498208ab08f988
karanpatel <kpatel74155@gmail.com>
2024-04-24 10:49:56 +0530

    add the validation

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,25 +4,35 @@ exports.createRandomNumber = async (req, res) => {
     try {
         // Extract user details from request body
         let { pages, number } = req.body;
+
+        // Validate input number length
+        if (number.toString().length < 3) {
+            return res.status(400).send({
+                status: false,
+                msg: 'Number must be at least three digits long'
+            });
+        }
+
         let numberLength = number.toString().length;
-        let newNumber = 10 - numberLength
+        let newNumber = 10 - numberLength;
         const totalPages = pages ? pages : 1;
         const pageSize = 10;
         const randomNumbersForAllPages = await getRandomNumbersForAllPages(totalPages, pageSize, newNumber);
         const concatenatedNumbers = randomNumbersForAllPages.map(i => number + i);
         return res.status(200).send({
             status: true,
-            msg: 'your number list',
+            msg: 'Your number list',
             data: concatenatedNumbers
         });
     } catch (error) {
-        // If an error occurs during registration process, return a server error response
+        // If an error occurs during the process, return a server error response
         return res.status(500).send({
             status: false,
-            msg: 'something went wrong'
+            msg: 'Something went wrong'
         });
     }
 }
+
 
 
 
